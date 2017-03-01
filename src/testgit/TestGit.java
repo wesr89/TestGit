df5ef9f75a2f3542c251803693862741357eb960
Wes <wredmond@squarespace.com>
2017-03-01 01:31:15 +0000

    Removed initial println statement from the main method

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -7,7 +7,7 @@ package testgit;
 public class TestGit {
 
     public static void main(String[] args) {
-        System.out.println("Story!?");
+        
         
         System.out.println("Second!?");
     }
